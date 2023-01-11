37d6931d27ebe262b2293c79084a0a392e189d17
Gary Kam <garyhkam@gmail.com>
2023-01-11 10:26:53 -0800

    randomize location of eye in spatial relationship game

--- app/src/main/java/io/github/garykam/atsaplus/ui/spatialrelationship/SpatialRelationshipScreen.kt
+++ app/src/main/java/io/github/garykam/atsaplus/ui/spatialrelationship/SpatialRelationshipScreen.kt
@@ -1,14 +1,14 @@
 package io.github.garykam.atsaplus.ui.spatialrelationship
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -16,24 +16,32 @@ import io.github.garykam.atsaplus.R
 
 @Composable
 fun SpatialRelationshipScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Eye()
-        Airplane(size = 90.dp)
-        Airplane(size = 200.dp)
+    Box(modifier = Modifier.fillMaxSize()) {
+        Eye(size = 160.dp)
+        Airplane(size = 80.dp)
+        Airplane(size = 160.dp)
     }
 }
 
 @Composable
-private fun Eye() {
-    val rotation = (Math.random() * 360).toFloat()
+private fun Eye(size: Dp) {
+    val configuration = LocalConfiguration.current
+    var x = configuration.screenWidthDp * Math.random()
+    if (x + size.value >= configuration.screenWidthDp) {
+        x -= size.value
+    }
+    var y = configuration.screenHeightDp * Math.random()
+    if (y + size.value >= configuration.screenHeightDp) {
+        y -= size.value
+    }
+    val rotation = (360 * Math.random()).toFloat()
     Image(
         painter = painterResource(id = R.drawable.eye),
         contentDescription = "eye",
-        modifier = Modifier.size(160.dp).rotate(rotation)
+        modifier = Modifier
+            .size(size)
+            .offset(x.dp, y.dp)
+            .rotate(rotation)
     )
 }
 
@@ -43,6 +51,8 @@ private fun Airplane(size: Dp) {
     Image(
         painter = painterResource(id = R.drawable.airplane),
         contentDescription = "airplane",
-        modifier = Modifier.size(size).rotate(rotation)
+        modifier = Modifier
+            .size(size)
+            .rotate(rotation)
     )
 }
